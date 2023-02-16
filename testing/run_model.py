bfe86c93db4e7becef16e10b9fe4958fdea1c8f9
hadarsnorlax <hadar.mirsky1@gmail.com>
2023-02-16 21:15:07 +0200

    Run the model on the image

---
+++ testing/run_model.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+from model import model
+
+IMAGE_PATH = r"C:\Users\hadar\Desktop\Projects\MAFAT-Challenge\testing\data\images\126_1280_5120.tiff"
+
+# Load the model
+model = model()
+model.load("model_url.txt")
+
+# Load the image
+img = cv2.imread(IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
+
+# Predict the objects in the image
+detections = model.predict(img)
+
+# Parse the output of the predict function to extract the detection information
+for i, cls in enumerate(model.CLASSES):
+    for detection in detections[i]:
+        confidence = detection[0]
+        coordinates = detection[1:]
+        x1, y1, x2, y2, x3, y3, x4, y4 = coordinates
+        # Do something with the detection information
